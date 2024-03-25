0b8c96a0c259b8f1a69bae00558fa27f6587336f
Subhajit <subhabera211@gmail.com>
2024-03-25 22:05:06 +0530

    banners : home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
-import React from 'react'
 import Header1 from '../components/Header1'
 import Header2 from '@/components/Header2'
 import Header3 from '@/components/Header3'
+import Image from "next/image";
 
 const Home = () => {
   return (
     <div>
-      <Header1/>
-      <Header2/>
-      <Header3/>
+      <Header1 />
+      <Header2 />
+      <Header3 />
+      <div className="mx-20">
+        <div className=" my-9">
+          <Image
+            src={"/banner1.avif"}
+            alt="banner1"
+            width={200}
+            height={200}
+            className=" h-60 w-full"
+          />
+        </div>
+        <div className="mb-14">
+          <Image
+            src={"/banner2.avif"}
+            alt="banner1"
+            width={200}
+            height={200}
+            className=" h-30 w-full"
+          />
+        </div>
+      </div>
     </div>
   )
 }
