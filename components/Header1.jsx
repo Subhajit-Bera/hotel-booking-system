ba94f45b800a187097d131b8c6bd7bb8899ca568
Subhajit <subhabera211@gmail.com>
2024-03-28 14:22:06 +0530

    Logo Modifiction

--- components/Header1.jsx
+++ components/Header1.jsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 const Header1 = () => {
     return (
         <div className=" flex justify-between border-b-2 border-gray-300 items-center h-24 px-10">
-            <Image
+            <Image 
                 src={"/logo.png"}
                 alt="logo"
                 width={200}
                 height={200}
-                className=" w-28 h-28 "
+                className=" w-28 h-34 mt-3 "
             />
+
             <div className=" h-full flex">
                 <Block imgSrc={"/m.png"} title={"Become a member"} para={"Additional 10% off on stays."} />
                 <Block imgSrc={"/busi.png"} title={"StayEase for business"} para={"Trusted by 5000 corporates"} />
