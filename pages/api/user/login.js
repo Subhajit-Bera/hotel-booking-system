932125b6f6d2e4dce30ed36f2c61b0fdc4779257
Subhajit <subhabera211@gmail.com>
2024-03-28 14:21:08 +0530

    Login and Register

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -2,7 +2,6 @@ import connectDB from "@/db";
 import User from "@/models/user-model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { use } from "moongose/routes";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
--- pages/login.jsx
+++ pages/login.jsx
@@ -1,8 +1,55 @@
+"use client";
 import Head from "next/head";
+import { useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const login = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [login, setLogin] = useState(true);
+
+    const router = useRouter();
+
+    const handleSignup = async () => {
+        const res = await axios.post(`/api/user/register`, {
+            name,
+            email,
+            password,
+        });
+        if (res?.data) {
+            // console.log(res);
+            Cookies.set("user", res.data.token, { expires: 7 });
+            toast.success(res.data.msg);
+            
+            router.back();
+        }
+    };
+
+    const handleToggle = () => {
+        setLogin(!login);
+    };
+
+    const handleLogin = async () => {
+        const res = await axios.post(`/api/user/login`, {
+            email,
+            password,
+        });
+        if (res?.data) {
+            Cookies.set("user", res.data.token, { expires: 7 });
+            // console.log(res);
+            toast.success(res.data.msg);
+            router.back();
+        }
+    };
+
+
     return (
-        <div className="flex h-screen justify-center items-center relative bg-login-background bg-no-repeat bg-cover opacity-75">
+        <div className="flex h-screen justify-center items-center relative bg-login-background bg-no-repeat bg-cover ">
             <Head>
                 <title>StayEase - Login !</title>
             </Head>
@@ -17,7 +64,7 @@ const login = () => {
                     <p className=" font-bold text-5xl text-justify  text-white">
                         There a smarter way to StayEase
                     </p>
-                    <p className=" text-2xl mt-5 text-justify text-white">
+                    <p className=" text-2xl mt-5 text-justify text-white font-bold">
                         Sign up with your phone number and get exclusive access to
                         discounts and savings on StayEase and with our many travel
                         partners.
@@ -29,44 +76,60 @@ const login = () => {
                     </p>
 
                     <div className="px-5">
-                        <h3 className=" text-3xl font-bold my-5">Login / Signup</h3>
+                        <h3 className=" text-3xl font-bold my-5">
+                            {login ? "Login" : "Signup"}
+
+                        </h3>
                         <p className=" font-bold text-md mb-1">
-                            Please enter your phone number to continue
+                            Please enter your email to continue
                         </p>
 
-                        <input
-                            type="text"
-                            placeholder="Enter your name..."
-                            className=" outline-none border my-3 border-black px-3 py-1 w-96 h-10"
-                        />
+                        {login ? ("") : (
+                            <input
+                                type="text"
+                                placeholder="Enter your name..."
+                                className=" outline-none border my-3 border-black px-3 py-1 w-96 h-10"
+                                onChange={(e) => setName(e.target.value)}
+
+                            />
+                        )
+                        }
+
                         <input
                             type="email"
                             placeholder="Enter your email..."
                             className=" outline-none border my-3 border-black px-3 py-1 w-96 h-10"
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                         <input
                             type="password"
                             placeholder="Enter your password..."
                             className=" outline-none border my-3 border-black px-3 py-1 w-96 h-10"
+                            onChange={(e) => setPassword(e.target.value)}
                         />
 
                         <button
                             type="submit"
                             className=" w-96 h-12 text-md font-bold bg-red-500 hover:cursor-pointer hover:bg-red-600 text-white my-5 rounded-lg"
+                            onClick={login ? handleLogin : handleSignup}
                         >
-                            Signup
+                            {login ? "Login " : " Sign Up"}
                         </button>
                         <p className="my-1 text-lg">
-                            <span>Already have an account?</span>
-                            <span className=" ml-1 border-b-2 border-red-500 text-red-600 pb-1 hover:cursor-pointer">
+                            <span>
+                                {login ? "Don`t have an account ?" : "Already have an account ?"}
+                            </span>
+                            <span className=" ml-1 border-b-2 border-red-500 text-red-600 pb-1 hover:cursor-pointer"
+                                onClick={handleToggle}>
                                 {" "}
-                                Login
+                                {" "}
+                                {login ? "Sign Up" : "Login"}
                             </span>
                         </p>
-
                     </div>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     )
 }
